d899903ec7347682002e87533193e9cfc457b3fc
jarg021 <31014497+jarg021@users.noreply.github.com>
2019-08-03 17:20:22 -0500

    delete coments

--- cards/main.go
+++ cards/main.go
@@ -1,10 +1,5 @@
 package main
-/*
-Aquí se podra ver dos diferentes tipos de estructuras basicas de Go
-Array y Slice
-un array tiene un tamaño determinado
-pero un slice es un array que puede crecer o reducirce a voluntad
-*/
+
 import (
 	"fmt"
 )
