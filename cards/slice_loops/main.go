3ac2734eb06f0807dabff76e477adda2de55e0e3
jarg021 <31014497+jarg021@users.noreply.github.com>
2019-08-03 16:34:53 -0500

    add

---
+++ cards/slice_loops/main.go
@@ -0,0 +1,18 @@
+package main
+/*
+Aquí se podra ver dos diferentes tipos de estructuras basicas de Go
+Array y Slice
+un array tiene un tamaño determinado
+pero un slice es un array que puede crecer o reducirce a voluntad
+*/
+import (
+	"fmt"
+)
+
+func main()  {
+	card:= []string{}
+} 
+
+func newCard()string  {
+	return "5 de diamantes"
+}
